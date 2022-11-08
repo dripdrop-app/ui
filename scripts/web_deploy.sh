1b1e107ca89dabfde6f6d8db3ecdd2fdd774a2c6
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-11-07 22:17:22 -0500

    add waiting for builds

--- scripts/web_deploy.sh
+++ scripts/web_deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+while [ -f $HOME/build.txt ]
+do
+    echo 'Waiting on build'
+    sleep 10
+done
+touch $HOME/build.txt
 mkdir -p build
 \cp -r $HOME/build $HOME/dripdrop
 cd $HOME/dripdrop
 docker compose -f docker-compose.yml build --progress plain
 docker compose -f docker-compose.yml down
 docker compose -f docker-compose.yml up -d
+rm $HOME/build.txt
